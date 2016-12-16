00e8480849b21c515cd4a1757d61f758ee0fbdc0
imalbert <albert.s.manuel@gmail.com>
2016-12-16 11:46:52 +0800

    Add scss loaders; bulma css; styles/styles.scss

--- src/entry.js
+++ src/entry.js
@@ -1,9 +1,28 @@
+import './styles/styles.scss'
 import React from 'react'
 import { render } from 'react-dom'
 
 render(
-  <div>
-    This is the app.
+  <div className='section'>
+    <div className='container'>
+      This is the app.
+
+      <div className='columns'>
+        <div className='column'>
+          <p className='notification is-info'>1</p>
+        </div>
+        <div className='column'>
+          <p className='notification is-success'>2</p>
+        </div>
+        <div className='column'>
+          <p className='notification is-warning'>3</p>
+        </div>
+        <div className='column'>
+          <p className='notification is-danger'>4</p>
+        </div>
+      </div>
+
+    </div>
   </div>,
   document.getElementById('app')
 )
